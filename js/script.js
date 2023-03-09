377a0f2e7b6b617dd224258a7141aa7ea98796ca
masudrahman-dev <masudrahman.dev@gmail>
2023-03-09 14:28:19 +0600

    fiexd calculation

--- js/script.js
+++ js/script.js
@@ -1,33 +1,54 @@
 const calculateExpense = () => {
-  const income = document.querySelector('#income').value;
+  // const income = Number(document.querySelector('#income').value);
+  // const income = Number(document.querySelector('#income').value);
   // console.log(income);
-  const food = document.querySelector('#food').value;
+  // const food = document.querySelector('#food').value;
   // console.log(food);
-  const rent = document.querySelector('#rent').value;
+  // const rent = document.querySelector('#rent').value;
   // console.log(rent);
-  const clothes = document.querySelectorAll('#clothes')[0].value;
+  // const clothes = document.querySelectorAll('#clothes')[0].value;
   // console.log(clothes);
-
+  const income = Number(document.querySelector('#income').value);
+  // console.log(income);
+  const food = Number(document.querySelector('#food').value);
+  // console.log(food);
+  const rent = Number(document.querySelector('#rent').value);
+  // console.log(rent);
+  const clothes = Number(document.querySelectorAll('#clothes')[0].value);
+  // console.log(clothes);
+  // const num = Number('');
+  // console.log('num :>> ', num);
   if (
+    // income === '' ||
+    // income < 0 ||
+    // food === '' ||
+    // food < 0 ||
+    // rent === '' ||
+    // rent < 0 ||
+    // clothes === '' ||
+    // clothes < 0
+    income === 0 ||
     income < 0 ||
-    income == '' ||
+    isNaN(income) ||
     food < 0 ||
-    food == '' ||
+    isNaN(food) ||
     rent < 0 ||
-    rent == '' ||
+    isNaN(rent) ||
     clothes < 0 ||
-    clothes == ''
+    isNaN(clothes)
   ) {
     alert('Inputs must be positive numbers');
     return;
   }
   // calculate expense
-  const expense = parseInt(food) + parseInt(rent) + parseInt(clothes);
+  // const expense = parseInt(food) + parseInt(rent) + parseInt(clothes);
+  const expense = food + rent + clothes;
 
   // calculate balance
-  const balance = parseInt(income.value) - expense;
+  // const balance = parseInt(income) - expense;
+  const balance = income - expense;
   //   validate income
-  if (expense > income.value) {
+  if (expense > income) {
     alert('Expenses cannot be more than income');
   } else {
     // view total expense and balance
@@ -36,25 +57,30 @@ const calculateExpense = () => {
   }
 };
 
+// calculate saving
 const calculateSavings = () => {
   // calculate saving amount
-  const savePercentage = document.getElementById('save').value;
+  const savePercentage = Number(document.getElementById('save').value);
+  console.log('savePercentage :>> ', savePercentage);
   //   Validate saving percentage value
-  if (savePercentage < 0) {
+  if (savePercentage < 0 || isNaN(savePercentage)) {
     alert('Provide positive saving value');
   }
-  const savingAmount = savePercentage / 100 + income;
-
+  const totalBalance = Number(document.getElementById('balance').innerText);
+  console.log('totalBalance :>> ', totalBalance);
+  // calculate saving amount
+  const savingAmount = (totalBalance * savePercentage) / 100;
+  console.log('savingAmount :>> ', savingAmount);
   // calculate remaining balance
-  const balance = document.getElementById('balance').innerText;
-  const remainingBalance = balance - savingAmount;
-
+  // const balance = document.getElementById('balance').innerText;
+  const remainingBalance = totalBalance - savingAmount;
+console.log('remainingBalance :>> ', remainingBalance);
   //   validate saving amount
-  if (savingAmount > balance) {
+  if (savingAmount > totalBalance) {
     alert('SavingAmount is greater than balance');
   } else {
     // view saving amount and remaining balance
     document.getElementById('saving-amount').innerText = savingAmount;
-    document.getElementById('remaining-balance').innerText = remainingBalance;
+    document.getElementById('remaining-balance').innerText = remainingBalance.toFixed(2);
   }
 };
